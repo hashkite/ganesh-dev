dbed8711eace5ae7a67d1654e89e74f79c00685d
Hashkite <sargar.akshay96@gmail.com>
2025-03-10 15:26:38 +0530

    Update build

--- App/src/pages/homePage.jsx
+++ App/src/pages/homePage.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 const DrupalContent = () => {
   const [landingPage, setLandingPage] = useState(null);
   const [components, setComponents] = useState([]);
+  const [included, setIncluded] = useState([]);
+  const [files, setFiles] = useState([]); // Store images separately
 
   useEffect(() => {
     fetch("https://dhangar.in.ddev.site/jsonapi/node/landing_page/2e5b621a-7f7b-4d2b-ba7e-45430f3c6488?include=field_components")
@@ -12,12 +14,13 @@ const DrupalContent = () => {
           setLandingPage(data.data); // Store landing page data
         }
         if (data.included) {
-          setComponents(data.included); // Store paragraph components
+          setIncluded(data.included);
+          setComponents(data.included.filter(item => item.type.includes("paragraph"))); // Filter only paragraph components
+          setFiles(data.included.filter(item => item.type === "file--file")); // Filter only file entities
         }
       })
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
-  console.log(components);
 
   if (!landingPage) return <p>Loading...</p>;
 
@@ -28,23 +31,29 @@ const DrupalContent = () => {
       <h3>Components:</h3>
       <ul>
         {components.map((component) => {
-          const image = components.find((img) => img.id === component.relationships.field_hero_image?.data?.id);
+          const heroImageId = component.relationships?.field_hero_image?.data?.id;
+          const media = included.find((item) => item.id === heroImageId);
+          
+          console.log(included);
+          console.log("Hero Image ID:", heroImageId);
+          console.log("Media Object:", media);
+
 
           return (
             <li key={component.id}>
-            <h4>{component.attributes.field_title}</h4>
-            <div dangerouslySetInnerHTML={{ __html: component.attributes.field_description.value }} />
-            
-            {image && (
-                <img
-                  src={`https://dhangar.in.ddev.site${image.attributes.uri.url}`}
-                  alt={component.attributes.field_title}
-                  width="600"
-                  loading="lazy"
+              <h4>{component.attributes.field_title}</h4>
+              <div dangerouslySetInnerHTML={{ __html: component.attributes.field_description?.value }} />
+              
+            {media?.attributes?.uri?.url && (
+              <img
+                src={`https://dhangar.in.ddev.site/sites/default/files/${media.attributes.uri.url.replace("public://", "")}`}
+                alt={component.attributes.field_title}
+                width="600"
+                loading="lazy"
                 />
-              )}
-          </li>
-          )
+            )}
+            </li>
+          );
         })}
       </ul>
     </div>
